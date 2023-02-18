a1984fa597112b4332917b1ff3dde782e77c5564
pasin2545 <keawninpasin@gmail.com>
2023-02-18 15:01:32 +0700

    Co-authored-by: XZ0T1Cz- <NopasitJ@users.noreply.github.com> Co-authored-by: 0xPoom <underpoom@users.noreply.github.com> Co-authored-by: Thanyalak A <Pthanzz@users.noreply.github.com>
    add back ground and change value input

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import { useState } from 'react'
+
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+
 
   const [baseImage, setBaseImage] = useState("");
 
@@ -26,38 +28,74 @@ function App() {
     });
   };
 
-  const [val, setVal] = useState("Hello there")
+  const [name, set_name_Val] = useState("")
+  const [price, set_price_Val] = useState("")
+  const [amount, set_amount_Val] = useState("")
+  const [description, set_Des_Val] = useState("")
   const click = () => {
-    alert(val)
+    alert(name)
   }
-  const change = event => {
-    setVal(event.target.value)
+  const change_name_event = event => {
+    set_name_Val(event.target.value)
   }
+  const change_price_event = event => {
+    set_price_Val(event.target.value)
+  }
+  const change_amount_event = event => {
+    set_amount_Val(event.target.value)
+  }
+  const change_Des_event = event => {
+    set_Des_Val(event.target.value)
+  }
+
+
+  
+  const [posts, setPosts] = useState([]);
+
+  function test_api({
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts?_limit=10')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setPosts(data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
+}
+  
+
   return (
+    test_api();
+
+
     <div spaceY={4} className='App'>
       <div className='br'>
-        <input onChange={change} value={val} placeholder='FFF' />
+        <input onChange={change_name_event} value={name} placeholder='Product Name' />
       </div>
       <div className='br'>
-        <input required name="name" placeholder='DDD' />
+        <input onChange={change_price_event} value={price} placeholder='Price' />
       </div>
       <div className='br'>
-        <input required name="name" placeholder='DDD' />
+        <input onChange={change_amount_event} value={amount} placeholder='amount' />
       </div>
       <div className='br'>
-        <input required name="name" placeholder='DDD' />
+        <input onChange={change_Des_event} value={description} placeholder='Description' />
       </div>
       <div class="br">
-        <label class="input-group-text" for="inputGroupSelect01">Options</label>
+        <label class="input-group-text" for="inputGroupSelect01">Catagory</label>
         <select class="form-select" id="inputGroupSelect01">
           <option selected>Choose...</option>
           <option value="1">One</option>
           <option value="2">Two</option>
           <option value="3">Three</option>
+
         </select>
       </div>
 
-      <div className="br">
+      <div className="imgbr">
         <input
           type="file"
           onChange={(e) => {
@@ -65,7 +103,7 @@ function App() {
           }}
         />
         <br></br>
-        <img src={baseImage} height="200px" />
+        <img src={baseImage} height="120px" width="150" />
       </div>
       <div className='br'>
         <button onClick={click}> Submit </button>
